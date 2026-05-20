7392c03eb691b14e98073a74cef93ce4f2cb1004
lcoombe <lauren.e.coombe@gmail.com>
2026-05-20 14:30:13 -0700

    Fix meson.build, prepare for release v1.0.7

--- bin/ntsynt_run.py
+++ bin/ntsynt_run.py
@@ -5,7 +5,7 @@ Run the minimizer graph stage of ntSynt
 import argparse
 from ntsynt_synteny import NtSyntSynteny
 
-NTSYNT_VERSION = 'ntSynt v1.0.6'
+NTSYNT_VERSION = 'ntSynt v1.0.7'
 
 def parse_arguments():
     "Parse arguments from argparse"
