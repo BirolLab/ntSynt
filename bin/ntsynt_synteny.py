2ba60ca7f3add7936d59ad0f1f2aeb5454e5dd8a
lcoombe <lauren.e.coombe@gmail.com>
2026-08-18 07:10:47 -0700

    Guard for edge case with no blocks

--- bin/ntsynt_synteny.py
+++ bin/ntsynt_synteny.py
@@ -454,6 +454,8 @@ class NtSyntSynteny(ntjoin.Ntjoin):
     def merge_collinear_blocks(self, blocks):
         "Merge collinear blocks that are less than threshold apart, and are not indels"
         out_blocks = []
+        if not blocks:
+            return out_blocks
         curr_block = blocks[0]
         for block in blocks[1:]:
             orientations = True # Are the orientations all the same?
